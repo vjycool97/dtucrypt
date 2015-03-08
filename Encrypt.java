86970387b185fd1a756906bddd0581fe366d0db2
unknown <shekhardtu@gmail.com>
2015-03-09 00:18:54 +0530

    first commit

--- Encrypt.java
+++ Encrypt.java
@@ -3,101 +3,319 @@ import acm.io.*;
 import acm.util.*;
 import java.awt.*;
 import acm.graphics.*;
+import acmx.export.javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 import javax.imageio.ImageIO;
-
-
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Encrypt extends ConsoleProgram {
 	
+	int Vrow = 26;
+	int Vcol = 26;
+	int Vtab[][] = new int[26][26];
+	String rsadecrypt;
+	int ref,valid;
+
 	public static void main(String[] args) {
-		new Encrypt().start(args);
-	}
+		new Encrypt().start(args);	}
 
 	public void init(){
+		
 		can = new HCanvas();
-		add(can);		
-	}
-	
-	
+		add(can);	
+
+//web1
+		JButton visit1=new JButton("DTUCrypt 404");
+		add(visit1,WEST);
+		visit1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e1) {openURL("http://localhost/minor/dtucrypt404.html"); }
+														} );
+//web2		
+		JButton visit2=new JButton("Caesar Cipher");
+		add(visit2,WEST);
+		visit2.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e2) {openURL("http://localhost/minor/caesar.html"); }
+														} );
+//web3
+				JButton visit3=new JButton("Vigenere");
+				add(visit3,WEST);
+				visit3.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e3) {openURL("http://localhost/minor/vigenere.html"); }
+																} );
+//web4		
+				JButton visit4=new JButton("RSA Encryption");
+				add(visit4,WEST);
+				visit4.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e4) {openURL("http://localhost/minor/rsa.html"); }
+																} );
+//web5
+				JButton visit5=new JButton("Image Steganography");
+				add(visit5,WEST);
+				visit5.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e5) {openURL("http://localhost/minor/image.html"); }
+																} );
+						}
 
 public void run(){
+	
+	for (int row = 0; row < Vrow; row++){
+		for (int col = 0; col < Vcol; col++){
+		Vtab[row][col] = (row + col) % 26;
+											}
+										}
+	
 	String chr="yes";
+	
 	while( chr.equals("yes")||chr.equals("YES")){
+can.Home();
+		println(" Welcome To DTUCrypt ! ");
+			println(" 1) Encrypt your text ? ");
+			println(" 2) Decrypt Your text ? ");
+			println(" Enter your choice : ");
 		
-		println("Welcome To iCrypt 101 !");
-		println(" 1) Encrypt Yout Text ?");
-		println(" 2) Decrypt Yout Text ?");
-		println(" 3) Decrypt Yout Image ?");
-		println("Enter Your Choice");
+			int ch1 = readInt();
 	
-	
-		int choice = readInt();
-	
-		if(choice==1){
-			can.Encrypt();
-			println("Please use only a-z, 0-9 and spaces ");
-			println("Special Character may lead to wrong message");
-			println("Enter the String to Be Encrypted");
-		
-			SR=Initialise();
-			println("what Do You want to Enccrypt to ");
-			println(" 1) If you want to Encrypt to TXT");
-			println(" 2) If you want to Encrypt to Image");
-			int ch = readInt();
-			if(ch==1){
-				int Key=rgen.nextInt(5,20);
-				ENC=EncryptA(SR,Key);
-				Print(ENC);
-				can.Etovr(Key);
+		if(ch1==1){
+			can.Unenc();
+			println(" Encrypt your data to ");
+			println(" 1) Text? ");
+			println(" 2) Image? ");
 			
-			}
-			if(ch==2){
+			int ch2 = readInt();
+			can.Encrypt();
+
+			if(ch2==1){
+				println(" via ");
+				println(" 1) Caesar Cipher? ");
+				println(" 2) Vigenere? ");
+				println(" 3) RSA? ");
+				println(" Note: Special characters will be retained unencrypted ");
+
+				int ch3 = readInt();
+				
+
+				//Encrypt via Caesar
+				if(ch3==1){
+					SR=Initialise();
+					int Key=rgen.nextInt(5,20);
+					ENC=EncryptA(SR,Key);
+					Print(ENC);
+					can.Etovr(Key);
+							}
+					
+				//Encrypt via Vigenere
+				else if(ch3==2){
+
+						println(" Enter the plaintext : ");
+						String plainText=readLine();
+						plainText = plainText.toUpperCase();
+						println(" Enter the key string : ");
+						String key = readLine();
+						key = key.toUpperCase();
+						String cipherText = "";
+						int keyIndex = 0;
+						for (int ptextIndex = 0; ptextIndex < plainText.length();ptextIndex++){
+								char pChar = plainText.charAt(ptextIndex);
+								int asciiVal = (int) pChar;
+								if (pChar == ' '){
+												cipherText += pChar;
+												continue;}
+								if (asciiVal < 65 || asciiVal > 90){
+															cipherText += pChar;
+															continue;}
+						int basicPlainTextValue = ((int) pChar) - 65;
+						char kChar = key.charAt(keyIndex);
+						int basicKeyValue = ((int) kChar ) - 65;
+						int tableEntry = Vtab[basicPlainTextValue][basicKeyValue];
+						char cChar = (char) (tableEntry + 65);
+						cipherText += cChar;
+						keyIndex++;
+						if (keyIndex == key.length())
+												keyIndex = 0;
+																						}
+						println(" Encrypted message is : "+cipherText);
+							}
+			
+				//Encrypt via RSA
+				else if(ch3==3){
+						rsaEncryption();}
+						}
+			
+			if(ch2==2){
+				
+				SR=Initialise();
 				int Key=rgen.nextInt(5,20);
-				println(""+Key);
 				ENC=EncryptA(SR,Key);
-				println("Enter the File Source :");
+				println(" Enter the file source :");
 				String path ;
 				path=readLine();
 				BufferedImage ENIMG =null;
 				ENIMG=EncryptImg(ENC,path,Key);
 				write(ENIMG);
 				can.Eovr();
+					}
+					}
+	
+	if(ch1==2){
+		can.StDec();
+		println(" Decrypt your data from ");
+		println(" 1) Text? ");
+		println(" 2) Image? ");
 
-			}
+		int ch2=readInt();
 		
+				
+		if(ch2==1){
+			
+				println(" via ");
+				println(" 1) Caesar Cipher? ");
+				println(" 2) Vigenere? ");
+				println(" 3) RSA? ");
 
-		}
-	
-		if(choice==2){
-			can.Decrypt();
-			println("Enter your Key?:");
-			int key=readInt();
-			println("Entered the coded txt?:");
-			SR=Initialise();
-			ENC=EncryptA(SR,-key);
-			Print(ENC);
-			can.Deovr();
-	
-			}
-	
-		if(choice==3){
-			can.Decrypt();
-			BufferedImage EImage =InitIM();
-			DecryptIMG(EImage);
-			can.Deovr();
-	
-			}
-		println("Do you wish to run the program again(yes/no)");
-		chr=readLine();
-		println(chr);
-	} 
+				int ch3=readInt();
+				
+				if(ch3==1){
+						println(" Enter your key : ");
+						int key=readInt();
+						SR=Initialise();
+						ENC=EncryptA(SR,-key);
+						Print(ENC);
+						can.Deovr();
+							}
+						
+
+				else if(ch3==2){
+							println(" Enter the encrypted text : ");
+							String cipherText = readLine();
+							cipherText = cipherText.toUpperCase();
+							print(" Enter the key string : ");
+							String key = readLine();
+							key = key.toUpperCase();
+							String plainText = "";
+							int keyIndex = 0;
+							for (int ctextIndex = 0; ctextIndex < cipherText.length();ctextIndex++){
+										char cChar = cipherText.charAt(ctextIndex);
+										int asciiVal = (int) cChar;
+										if (cChar == ' '){
+											plainText += cChar;
+											continue;
+														}
+										if (asciiVal < 65 || asciiVal > 90){
+												plainText += cChar;
+												continue;
+														}
+							int basiccipherTextValue = ((int) cChar) - 65;
+							char kChar = key.charAt(keyIndex);
+							int basicKeyValue = ((int) kChar ) - 65;
+							for (int pIndex = 0; pIndex < Vcol; pIndex++){
+								if (Vtab[basicKeyValue][pIndex] == basiccipherTextValue){
+									char potcChar = (char)(Vtab[basicKeyValue][pIndex]	 + 65);
+									char potpChar = (char) (pIndex + 65);
+									plainText += potpChar;
+																						}
+																			}
+							keyIndex++;
+							if (keyIndex == key.length())
+							keyIndex = 0;
+							}
+						println(" Decrypted message is : "+plainText);
+						can.Deovr();
+						}
+						
+				if(ch3==3){
+					println(" Enter your reference number : ");
+					ref=readInt();
+					if(ref==valid)
+						println(" The decrypted text : "+rsadecrypt);
+					else println(" Wrong reference number! ");
+					can.Deovr();
 }
 
+		}				
+			if(ch2==2){
+				can.Decrypt();
 
+				BufferedImage EImage =InitIM();
+				DecryptIMG(EImage);
+				can.Deovr();
+				}
+		}
+println(" Do you wish to run the program again(yes/no)? ");
+chr=readLine();
+}
+}
+BigInteger p;  
+BigInteger q;  
+BigInteger N;  
+BigInteger phi;  
+BigInteger e;  
+BigInteger d; 
+int bitlength = 1024;  
+int blocksize = 256;
+Random r = new Random();
+String path;
+
+
+//RSA encryption starts here...
+
+private void rsaEncryption() {
+	println(" Enter the plain text : ");
+	String teststring=readLine();
+	     
+        
+        p = BigInteger.probablePrime(bitlength, r); 
+      
+        q = BigInteger.probablePrime(bitlength, r);  
+          N = p.multiply(q);  
+            
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));  
+          e = BigInteger.probablePrime(bitlength/2, r);  
+          
+        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0 ) {  
+            e.add(BigInteger.ONE);  
+        }  
+ d = e.modInverse(phi);   
+ println(" Encrypting String : " + teststring);  
+ //println("teststring.getbytes"+teststring.getBytes());
+ //println("String in Bytes: " + bytesToString(teststring.getBytes()));
+ byte[] encrypted = encrypt(teststring.getBytes());                    
+ //println("Encrypted String in Bytes: " + bytesToString(encrypted));  
+   ref=r.nextInt(100);
+   valid=ref;
+   println(" The reference number for ypour encrypted text : "+ ref);
+ byte[] decrypted = decrypt(encrypted);        
+ //println("Decrypted String in Bytes: " +  bytesToString(decrypted));  
+ rsadecrypt=new String(decrypted);
+ 
+}
+private static String bytesToString(byte[] encrypted) {  
+    String test = "";  
+    for (byte b : encrypted) {  
+        test += Byte.toString(b);  
+    }  
+    return test;  
+}  
+public byte[] encrypt(byte[] message) {       
+    return (new BigInteger(message)).modPow(e, N).toByteArray();  
+}
+public byte[] decrypt(byte[] message) {  
+    return (new BigInteger(message)).modPow(d, N).toByteArray();  
+} 
+//decrypt image starts  here...
 
 public void DecryptIMG (BufferedImage DEM){
 	
@@ -225,10 +443,9 @@ private BufferedImage EncryptImg(ArrayList<String> EN,String path,int key){
 	
 }
 
-
 public BufferedImage InitIM(){
-	println("Enter the Source");
-	String path=readLine();
+	println(" Enter the source image name : ");
+	path=readLine();
 	BufferedImage src = null;
 	try {
 		src = ImageIO.read(new File(path));
@@ -237,15 +454,14 @@ public BufferedImage InitIM(){
 	}
 	return src;
 }
-	
+
 public void write(BufferedImage ENIMG){
 	String ext="bmp";
-	String fileName = "Encrypted";
-	    File file = new File(fileName + "." + ext);
+	    File file = new File("encrypted." + ext);
 	    try {
 	        ImageIO.write((RenderedImage)ENIMG, ext, file);  // ignore returned boolean
 	    } catch(IOException e) {
-	        System.out.println("Write error for " + file.getPath() +
+	        System.out.println(" Write error for " + file.getPath() +
 	                           ": " + e.getMessage());
 	    }
 }
@@ -257,7 +473,7 @@ private ArrayList<String> Initialise(){
 	String str;
 	int cnt=0;
 	while(true){
-		println("Enter The Message ");
+		println(" Enter the plaintext message ");
 		str=readLine();
 		if(str.equals("")) 
 			break;
@@ -293,15 +509,37 @@ private void Print(ArrayList<String> EN){
 	
 	for(int i=0; i<EN.size();i++){
 		String Str=EN.get(i);
-		println("Encrypted : "+Str);
+		println(" Encrypted : "+Str);
 	
 	}
 	
 }
 
+
+
+
+public static void openURL(String url) {
+	String osName = System.getProperty("os.name");
+	try {
+	if (osName.startsWith("Windows"))
+	Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+	else { 
+	String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+	String browser = null;
+	for (int count = 0; count < browsers.length && browser == null; count++)
+	if (Runtime.getRuntime().exec(new String[] {"which", browsers[count]}).waitFor() == 0)
+	browser = browsers[count];
+	Runtime.getRuntime().exec(new String[] {browser, url});
+	}
+	}
+	catch (Exception e) {
+	JOptionPane.showMessageDialog(null, " Error in opening browser! " + ":\n" + e.getLocalizedMessage());
+	}
+	}
 HCanvas can;
 ArrayList<String> ENC = new ArrayList<String>();
 private ArrayList<String> SR = new ArrayList<String>();
-private RandomGenerator rgen = new RandomGenerator();	
+private RandomGenerator rgen = new RandomGenerator();
+
 
 }
